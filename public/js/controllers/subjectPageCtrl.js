766d512b635f6cb7a774544df2aea968180c68a3
Amanda Olczak <aolczak@gmail.com>
2017-02-20 07:02:49 -0500

    styling for subject pages

--- public/js/controllers/subjectPageCtrl.js
+++ public/js/controllers/subjectPageCtrl.js
@@ -4,28 +4,28 @@ app.controller("subjectPageCtrl", function($scope, $state) {
   $scope.servicesPage = {
     title: "Services",
     introText: "Service content will go here. This will be a one-page section and will not require additional routing. This may use modals to display additional information.",
-    moreInformation: "",
+    moreInformation: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi temp",
     modalContent: ""
   };
 
   $scope.projectsPage = {
     title: "Projects",
     introText: "An engaging yet brief introduction to the work ... maybe include various specialties. Some content from biography might be better suited for this section. Mostly we want to capture the user’s attention and encourage them to click below.",
-    moreInformation: "",
+    moreInformation: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi temp.",
     modalContent: ""
   };
 
   $scope.teamPage = {
     title: "Team",
     introText: "Team content will go here. This will be a one-page section and will not require additional routing. This may use modals to display additional information.",
-    moreInformation: "",
+    moreInformation: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi temp.",
     modalContent: ""
   };
 
   $scope.biographyPage = {
     title: "Biography",
     introText: "Anne R. Goulet has over 20 years of project management and construction administration experience in the architecture, planning, and design industry.",
-    moreInformation: "",
+    moreInformation: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi temp.",
     modalContent: ""
   };
 
