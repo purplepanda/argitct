4635e73e1d76f10fa5d6bb4e4b7a550a02a1dfdc
Amanda Olczak <aolczak@gmail.com>
2017-03-05 12:17:17 -0500

    landing page styling changes

--- public/js/controllers/homeCtrl.js
+++ public/js/controllers/homeCtrl.js
@@ -2,6 +2,7 @@ app.controller("homeCtrl", function($scope, $state) {
 
   $scope.homePage = {
     title: "Home",
+    logo: "images/logo/arg_burntlilac.svg",
     introText: "Argitct mission statement will go here. This may use modals to display additional information. There will also be another nav menu at the bottom of this page."
   };
 
--- public/js/controllers/landingCtrl.js
+++ public/js/controllers/landingCtrl.js
@@ -1,5 +1,7 @@
 app.controller("landingCtrl", function($scope, $state) {
 
+  $scope.title = "Project Strategy | Management | Representation"
   $scope.location = "New York, NY";
+  $scope.logo = "images/logo/arg_white.svg";
 
 });
