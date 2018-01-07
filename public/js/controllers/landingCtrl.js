ec7963fd67daacc86ebe94b4e40cf4df4bf1a333
Amanda Olczak <aolczak@gmail.com>
2018-01-06 22:15:50 -0500

    landing page styling

--- public/js/controllers/landingCtrl.js
+++ public/js/controllers/landingCtrl.js
@@ -1,14 +1,12 @@
 app.controller("landingCtrl", function($scope, $state) {
-
   $scope.title = "Project Strategy + Management Consulting";
   $scope.location = "New York, NY";
   $scope.logo = "images/logo/arg_white.svg";
   $scope.landingFadeIn = $('#landing-fadein');
   $scope.landingFadeOut = $('#landing-fadeout');
-  $scope.enterIcon = $('.landing-plus');
-
+  $scope.enterIcon = $('.icon-enter');
 
   $scope.landingFadeIn.hide().delay(1000).fadeIn(3000).removeClass('hidden');
   $scope.landingFadeOut.delay(250).fadeOut(2000);
-  $scope.enterIcon.hide().delay(2750).fadeIn(500).removeClass('hidden');
+  // $scope.enterIcon.delay(3000).removeClass('hidden');
 });
